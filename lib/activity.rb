7d4cd1e0256c4c354da80c0ad20955a83a9e26df
Eliot Swank <eliotswank@gmail.com>
2018-02-21 09:34:32 -0700

    initial commit, working on iter 1

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,13 @@
 class Activity
+  attr_reader :name,
+              :participants
+
+  def initialize(name, participants)
+    @name = name
+    @participants = {}
+  end
+
+  # def participants(name)
+  #   @participants
+  # end
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,23 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+
+  def test_it_exists
+    activity = Activity.new("hiking", {name: "Dave", paid: "50"})
+
+    assert_instance_of Activity, activity
+  end
+
+  def test_it_can_take_a_name
+    activity = Activity.new("hiking", {name: "Dave", paid: "50"})
+
+    assert_equal "hiking", activity.name
+  end
+
+  def test_it_has_participants
+    activity = Activity.new("hiking", {name: "Dave", paid: "50"})
+    expected = {name: "Dave", paid: "50"}
+    assert_equal expected, activity.participants({name: "Dave", paid: "50"})
+
+  end
 end
